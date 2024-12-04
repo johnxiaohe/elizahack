ddc0ecd25b7005208deeb7159aa81c97a4c1d860
johnxiaohe <1154910941@qq.com>
2024-12-05 00:11:50 +0800

    aaa

--- packages/client-telegram/src/messageManager.ts
+++ packages/client-telegram/src/messageManager.ts
@@ -309,13 +309,13 @@ export class MessageManager {
     }
 
     private async loopMsgListiner() {
+        var length = 0
         while(true) {
-            var length = 0
             await this.runtime.messageManager.getMemoriesByRoomIds({roomIds: ["db86f761-6fdc-016f-b4fa-48e11ef2a23b"]}).then((memories) => {
                 if (memories.length > length) {
                     length = memories.length
                     var lastMemory = memories[length - 1]
-                    elizaLogger.info(lastMemory.roomId, lastMemory.agentId, lastMemory.userId, JSON.stringify(lastMemory.content) )
+                    elizaLogger.info(JSON.stringify(lastMemory.content))
                 }
             });
             await new Promise(resolve => setTimeout(resolve, 1000)); // Pause for 1 second
